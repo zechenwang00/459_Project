6b50b1d7221ffacf3e11d99dbb69553f4bcd3c29
Zechen Wang <zechenwa@usc.edu>
2022-04-19 19:10:04 -0700

    bug fixes

--- SSD_main/oled.c
+++ SSD_main/oled.c
@@ -14,7 +14,7 @@ output short: selected recyclable type
 2 - Card Boxes
 3 - Paper
 */
-void showMenu(uint_t addr, short recyclable){
+void showMenu(uint8_t addr, short recyclable){
     SSD1306_ClearScreen ();
     SSD1306_SetPosition(0,1);
     SSD1306_DrawString("---Select Recyclable---");
@@ -24,52 +24,53 @@ void showMenu(uint_t addr, short recyclable){
         SSD1306_SetPosition(0,2);
         SSD1306_DrawString("0. Plastic Bottles <-");
         SSD1306_SetPosition(0,3);
-        SSD1306_DrawString("1. Glass Bottles");
+        SSD1306_DrawString("1. Glass Bottles   ");
         SSD1306_SetPosition(0,4);
-        SSD1306_DrawString("2. Card Boxes");
+        SSD1306_DrawString("2. Card Boxes   ");
         SSD1306_SetPosition(0,5);
-        SSD1306_DrawString("3. Paper");
+        SSD1306_DrawString("3. Paper   ");
         break;
     case 1:
         SSD1306_SetPosition(0,2);
-        SSD1306_DrawString("0. Plastic Bottles");
+        SSD1306_DrawString("0. Plastic Bottles   ");
         SSD1306_SetPosition(0,3);
         SSD1306_DrawString("1. Glass Bottles <-");
         SSD1306_SetPosition(0,4);
-        SSD1306_DrawString("2. Card Boxes");
+        SSD1306_DrawString("2. Card Boxes   ");
         SSD1306_SetPosition(0,5);
-        SSD1306_DrawString("3. Paper");
+        SSD1306_DrawString("3. Paper   ");
+        break;
     case 2:
-        case 1:
         SSD1306_SetPosition(0,2);
-        SSD1306_DrawString("0. Plastic Bottles");
+        SSD1306_DrawString("0. Plastic Bottles   ");
         SSD1306_SetPosition(0,3);
-        SSD1306_DrawString("1. Glass Bottles");
+        SSD1306_DrawString("1. Glass Bottles   ");
         SSD1306_SetPosition(0,4);
         SSD1306_DrawString("2. Card Boxes <-");
         SSD1306_SetPosition(0,5);
-        SSD1306_DrawString("3. Paper");
+        SSD1306_DrawString("3. Paper   ");
+        break;
     case 3:
         SSD1306_SetPosition(0,2);
-        SSD1306_DrawString("0. Plastic Bottles");
+        SSD1306_DrawString("0. Plastic Bottles   ");
         SSD1306_SetPosition(0,3);
-        SSD1306_DrawString("1. Glass Bottles");
+        SSD1306_DrawString("1. Glass Bottles   ");
         SSD1306_SetPosition(0,4);
-        SSD1306_DrawString("2. Card Boxes");
+        SSD1306_DrawString("2. Card Boxes   ");
         SSD1306_SetPosition(0,5);
         SSD1306_DrawString("3. Paper <-");
+        break;
     default:
         SSD1306_SetPosition(0,2);
         SSD1306_DrawString("0. Plastic Bottles <-");
         SSD1306_SetPosition(0,3);
-        SSD1306_DrawString("1. Glass Bottles");
+        SSD1306_DrawString("1. Glass Bottles   ");
         SSD1306_SetPosition(0,4);
-        SSD1306_DrawString("2. Card Boxes");
+        SSD1306_DrawString("2. Card Boxes   ");
         SSD1306_SetPosition(0,5);
-        SSD1306_DrawString("3. Paper");
+        SSD1306_DrawString("3. Paper   ");
         break;
     }
-    SSD1306_UpdateScreen (addr);
 }
 
 int main(void){
@@ -83,21 +84,22 @@ int main(void){
     DDRC &= ~(1 << PC1);
 
     // init ADC
-    // ADMUX: ref=AVCC, result=8bit, input=ADC1
-    ADMUX &= ~(1 << REFS1 | 1 << MUX3 | 1 << MUX2 | 1 << MUX1);
-    ADMUX |=  (1 << REFS0 | 1 << ADLAR | 1 << MUX0);
+    // ADMUX: ref=AVCC, result=10bit, input=ADC1
+    ADMUX &= ~(1 << REFS1 | 1 << ADLAR | 1 << MUX3 | 1 << MUX2 | 1 << MUX1);
+    ADMUX |=  (1 << REFS0 | 1 << MUX0);
     // ADCSRA: prescalar=128
     ADCSRA |= (1 << ADEN | 1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0);
 
     // init vars
-    unsigned char pressure;
-    char pressure_str[4];
+    unsigned short pot;
+    char rec_str[1];
+    // char pressure_str[4];
 
     while(1) {
         // read from ADC
         ADCSRA |= (1 << ADSC);
         while ((ADCSRA & (1 << ADSC)) != 0);
-        pot = ADCH;
+        pot = ADC;
         // sprintf(pressure_str, "%d", pressure);
 
         short rec = 0;
@@ -119,6 +121,13 @@ int main(void){
         }
         showMenu(addr, rec);
 
+        // print debug info
+        sprintf(rec_str, "%d", rec);
+        SSD1306_SetPosition(0,7);
+        SSD1306_DrawString(rec_str);
+
+        // update screen and sleep
+        SSD1306_UpdateScreen(addr);
         _delay_ms(50);
     }
 
